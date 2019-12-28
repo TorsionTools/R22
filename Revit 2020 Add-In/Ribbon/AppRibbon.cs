87bd7f43d56b9e45c2da939adadd257433da344e
Sean <sjp23@att.net>
2019-12-27 23:44:49 -0600

    Added the Sheet Selection Command to the RIbbon Panel 2

--- Revit 2020 Add-In/Ribbon/AppRibbon.cs
+++ Revit 2020 Add-In/Ribbon/AppRibbon.cs
@@ -26,20 +26,24 @@ namespace Revit_2020_Add_In.Ribbon
             RibbonPanel RibbonPanelOne = application.CreateRibbonPanel(TabName, "PANEL 1");
             RibbonPanel RibbonPanelTwo = application.CreateRibbonPanel(TabName, "PANEL 2");
 
-            //Create Push Button Data to creat the Push button from
+            //Create Push Button Data to create the Push button from
             PushButtonData pbdTestButton = new PushButtonData("cmdTestButton", "Button Name", AssemblyPath, "Revit_2020_Add_In.Commands.HelloWorld");
+            PushButtonData pbdSheetSelection = new PushButtonData("cmdSheetSelection", "Sheet" + Environment.NewLine + "Selector", AssemblyPath, "Revit_2020_Add_In.Commands.TSheetSelection");
 
             //Create a Push Button from the Push Button Data
             PushButton pbTestButton = RibbonPanelOne.AddItem(pbdTestButton) as PushButton;
+            PushButton pbSheetSelection = RibbonPanelTwo.AddItem(pbdSheetSelection) as PushButton;
 
             //Set Button Image
             pbTestButton.LargeImage = ButtonImage;
 
             //Set Button Tool Tip
             pbTestButton.ToolTip = "Tell the user what your button does here";
+            pbSheetSelection.ToolTip = "Select from all of the Sheets in the project";
 
             //Set Button Long description which is the text that flys out when you hover on a button longer
             pbTestButton.LongDescription = "Give the user more information about how they need to use the button features";
+            pbSheetSelection.LongDescription = "The Sheet Selection Form can be used in multiple ways to allow the user to select one or multiple sheets in the project and perform additiona actions on the sheets returned.";
         }
     }
 }
