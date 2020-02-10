761672d90105ce8325edf5beb69a009875de483d
Sean <sjp23@att.net>
2020-02-10 06:40:37 -0600

    Updated Versioning

--- Revit 2020 Add-In/Properties/AssemblyInfo.cs
+++ Revit 2020 Add-In/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("2020.1.25.0")]
+[assembly: AssemblyFileVersion("2020.2.10.0")]
