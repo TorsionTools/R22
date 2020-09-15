b60bae79b80e28c62616007b8bb1057b0974b1da
Sean <sjp23@att.net>
2020-09-15 12:25:49 -0500

    Included a check for active document to prevent exceptions if the toolbar is selected before opening document.

--- Revit 2020 Add-In/Ribbon/RibbonAvailability.cs
+++ Revit 2020 Add-In/Ribbon/RibbonAvailability.cs
@@ -10,6 +10,9 @@ namespace TorsionTools.Ribbon
         //Checks to see if the active document is a family or project
         public bool IsCommandAvailable(UIApplication applicationData, CategorySet selectedCategories)
         {
+            //Make sure there is an active document
+            if (applicationData.ActiveUIDocument == null)
+                return false;
             Document _doc = applicationData.ActiveUIDocument.Document;
             if (_doc.IsFamilyDocument)
             {
@@ -27,6 +30,9 @@ namespace TorsionTools.Ribbon
         //Checks to see if the active view is a sheet
         public bool IsCommandAvailable(UIApplication applicationData, CategorySet selectedCategories)
         {
+            if (applicationData.ActiveUIDocument == null)
+                return false;
+
             if (applicationData.ActiveUIDocument.Document.ActiveView.ViewType == ViewType.DrawingSheet)
             {
                 return true;
@@ -43,6 +49,9 @@ namespace TorsionTools.Ribbon
         //Checks to see if the active view is a floor plan
         public bool IsCommandAvailable(UIApplication applicationData, CategorySet selectedCategories)
         {
+            if (applicationData.ActiveUIDocument == null)
+                return false;
+
             if (applicationData.ActiveUIDocument.Document.ActiveView.ViewType == ViewType.FloorPlan)
             {
                 return true;
@@ -59,6 +68,9 @@ namespace TorsionTools.Ribbon
         //Checks to see if the active view is a Plan View of some kind
         public bool IsCommandAvailable(UIApplication applicationData, CategorySet selectedCategories)
         {
+            if (applicationData.ActiveUIDocument == null)
+                return false;
+
             if (applicationData.ActiveUIDocument.Document.ActiveView.ViewType == ViewType.FloorPlan || applicationData.ActiveUIDocument.Document.ActiveView.ViewType == ViewType.CeilingPlan || applicationData.ActiveUIDocument.Document.ActiveView.ViewType == ViewType.AreaPlan || applicationData.ActiveUIDocument.Document.ActiveView.ViewType == ViewType.EngineeringPlan)
             {
                 return true;
@@ -75,6 +87,9 @@ namespace TorsionTools.Ribbon
         //Checks to see if the active view is a "live" view and not a 3D, drafting, sheet or schedule
         public bool IsCommandAvailable(UIApplication applicationData, CategorySet selectedCategories)
         {
+            if (applicationData.ActiveUIDocument == null)
+                return false;
+
             View view = applicationData.ActiveUIDocument.Document.ActiveView;
             if (view is View3D || view is ViewDrafting || view is ViewSheet || view is ViewSchedule)
             {
@@ -92,6 +107,9 @@ namespace TorsionTools.Ribbon
         //Checks to see if the active view is anything other than a 3D, Sheet or schedule
         public bool IsCommandAvailable(UIApplication applicationData, CategorySet selectedCategories)
         {
+            if (applicationData.ActiveUIDocument == null)
+                return false;
+
             View view = applicationData.ActiveUIDocument.Document.ActiveView;
             if (view is View3D || view is ViewSheet || view is ViewSchedule)
             {
